c108e2a62a61a74b39d414eb59086ba38cfb8b2e
Anushka Tawte <anushka.tawte@gmail.com>
2020-07-19 13:15:43 +0530

    resolved

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 },
- () => console.log('connected to Db')
+//  () => console.log('connected to Db')
+ () => console.log(process.env.MONGO_URI )
 );
 
 //importing routes
